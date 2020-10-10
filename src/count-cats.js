cc08397812bb55d24cf900f9383cede3becf5ab7
DenisRebenock <denmaloyreb@mail.ru>
2020-10-10 23:47:27 +0300

    feat: count cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,12 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(inspectArr) {
+    throw new CustomError('Not implemented');
+    let secArr = [];
+
+    inspectArr.forEach((item) => secArr = secArr.concat(item));
+
+    let filtered = secArr.filter(currentValue => currentValue === '^^');
+
+    return filtered.length;
 };
