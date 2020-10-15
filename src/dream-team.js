ef50a88f66fd574bccafd6cb21449b71ebaf70fb
DenisRebenock <denmaloyreb@mail.ru>
2020-10-15 17:00:06 +0300

    feat: dream team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,25 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+
+    let member;
+    let resultArr = [];
+    let resultTeam = '';
+
+
+    for (i = 0; i < members.length; i++) {
+
+
+        if (typeof(members[i]) === 'string') {
+            member = members[i];
+            resultArr.push(member[0]);
+        }
+
+
+
+    }
+    resultArr = resultArr.sort();
+    resultTeam = resultArr.join('');
+
+    return (resultTeam);
 };
